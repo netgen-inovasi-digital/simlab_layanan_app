3606ba054ba5aad158f992cc3b3598aee4323634
Hal <zidaniversal@gmail.com>
2025-09-15 14:47:58 +0800

    Revert "feat: menambahkan table riwayat pembayaran"
    This reverts commit e5c4c22e6ddf170d20f6cd16d84ed09816296279.

--- appengines/app/Config/Database.php
+++ appengines/app/Config/Database.php
@@ -29,7 +29,7 @@ class Database extends Config
         'hostname'     => 'localhost',
         'username'     => 'root',
         'password'     => '',
-        'database'     => 'layanan_simlab',
+        'database'     => 'db_template',
         'DBDriver'     => 'MySQLi',
         'DBPrefix'     => '',
         'pConnect'     => false,
