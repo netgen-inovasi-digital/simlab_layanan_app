6017b716ec23d0208ded85522592f58da286e628
FaisalBAkbar544 <faisalrrakbar@gmail.com>
2025-11-03 19:04:03 +0800

    penyesuaian env

--- appengines/app/Modules/Pelayanan/Views/v_kuesioner_form_user.php.php
+++ appengines/app/Modules/Pelayanan/Views/v_kuesioner_form_user.php.php
@@ -1,4 +1,4 @@
-<div class="row">
+<!-- <div class="row">
     <div class="col-md-12">
         <div class="card">
             <div class="card-header d-flex justify-content-between align-items-center">
@@ -221,4 +221,4 @@
                 hideLoading();
             });
     }
-</script>
+</script> -->
