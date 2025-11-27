ca5441c3c112f2fd73159aa00a0eb5a8c9b92c12
FaisalBAkbar544 <faisalrrakbar@gmail.com>
2025-11-27 23:35:15 +0800

    penyesuaian env

--- appengines/app/Config/Database.php
+++ appengines/app/Config/Database.php
@@ -41,7 +41,7 @@ class Database extends Config
         'compress'     => false,
         'strictOn'     => false,
         'failover'     => [],
-        'port'         => 3306,
+        'port'         => 3308,
         'numberNative' => false,
         'foundRows'    => false,
         'dateFormat'   => [
