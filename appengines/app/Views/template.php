3661f40773903b08bade9f548f4217e519054492
FaisalBAkbar544 <faisalrrakbar@gmail.com>
2026-01-08 22:18:39 +0800

    perbaikan diksi browser

--- appengines/app/Views/template.php
+++ appengines/app/Views/template.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Template | Netx Template</title>
+    <title>Layanan Terpadu ULM</title>
     <link rel="icon" type="image/x-icon" href="<?= base_url('favicon.ico') ?>">
     <link rel="stylesheet" href="<?= asset_url('assets/css/bootstrap.min.css') ?>">
     <link rel="stylesheet" href="<?= asset_url('assets/css/bootstrap-icons.min.css') ?>">
