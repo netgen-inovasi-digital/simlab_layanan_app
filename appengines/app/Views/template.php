288180fb276fe30f829442322cb1f342709c57c9
FaisalBAkbar544 <faisalrrakbar@gmail.com>
2026-01-08 21:52:02 +0800

    revert logo

--- appengines/app/Views/template.php
+++ appengines/app/Views/template.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Template | Netx Template</title>
-    <link rel="icon" type="image/x-icon" href="/netx/favicon.ico">
+    <link rel="icon" type="image/x-icon" href="<?= base_url('favicon.ico') ?>">
     <link rel="stylesheet" href="<?= asset_url('assets/css/bootstrap.min.css') ?>">
     <link rel="stylesheet" href="<?= asset_url('assets/css/bootstrap-icons.min.css') ?>">
     <link rel="stylesheet" href="<?= asset_url('assets/css/style.css') ?>">
